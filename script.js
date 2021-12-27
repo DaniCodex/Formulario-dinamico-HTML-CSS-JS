b508d51963d39c5bb5275a3a8f3441c5c0ef179d
revolutiondaniel19@gmail.com <revolutiondaniel19@gmail.com>
2021-12-27 17:45:45 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+const $btnSignIn= document.querySelector('.sign-in-btn'),
+      $btnSignUp = document.querySelector('.sign-up-btn'),  
+      $signUp = document.querySelector('.sign-up'),
+      $signIn  = document.querySelector('.sign-in');
+
+document.addEventListener('click', e => {
+    if (e.target === $btnSignIn || e.target === $btnSignUp) {
+        $signIn.classList.toggle('active');
+        $signUp.classList.toggle('active')
+    }
+});
